10ed95d1c0f8b3f8854ab886eab3da2093501804
Quinta Helmer <qhelmer@5-MEST-08-0093.(none)>
2012-02-17 14:53:38 +0100

    Fix running chrX instead of autosome and vice versa

--- python/loaddata.py
+++ python/loaddata.py
@@ -31,16 +31,17 @@ def fsplitChromosomes (prefix,outputDir,logonly,chrX):
     if logonly==False:
         os.system(pedcommand)
     if chrX:
+        plinkcommand=" qsub.pl --jid "+jidfile+" -- plink "+inputOption+prefix+" --chr 23 --recode --out "+outputDir+"/"+prefix+"_chr23"
+        if logonly==False:
+            os.system(plinkcommand)
+        specfiles.append({'name' : outputDir+"/"+prefix+"_chr23", 'chromosome' : "23" })
+        print test
+    else:
         for chrom in range(1,24):
             plinkcommand=" qsub.pl --jid "+jidfile+" -- plink "+inputOption+prefix+" --chr "+str(chrom)+" --recode --out "+outputDir+"/"+prefix+"_chr"+str(chrom)
             if logonly==False:
                 os.system(plinkcommand)
             specfiles.append({'name' : outputDir+"/"+prefix+"_chr"+str(chrom), 'chromosome' : str(chrom) })
-    else:
-        plinkcommand=" qsub.pl --jid "+jidfile+" -- plink "+inputOption+prefix+" --chr 23 --recode --out "+outputDir+"/"+prefix+"_chr23"
-        if logonly==False:
-            os.system(plinkcommand)
-        specfiles.append({'name' : outputDir+"/"+prefix+"_chr23", 'chromosome' : "23" })
     return specfiles
 
 ##main

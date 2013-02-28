32f3b8f44182f0789dc6ab1a2feddbfb4b973b94
Stefan Boehringer <github@s-boehringer.org>
2013-02-28 13:22:36 +0100

    debug

--- R/testCoxPerSnp.R
+++ R/testCoxPerSnp.R
@@ -6,10 +6,10 @@
 library(survival)
 testCoxPerSnp = function(data, formula1, formula0, snp, ...) {
 	m1 = coxph(as.formula(formula1), data = data);
+print(summary(m1));
 	m0 = coxph(as.formula(formula0), data = data);
+print(summary(m0));
 
-print(m1);
-print(m0);
 	r = as.list(c(
 		coefficients(m0),
 		sqrt(diag(m0$var)),
@@ -24,7 +24,6 @@ print(m0);
 		paste('sd1', names(coefficients(m1)), sep = '.'),
 		'P-value'
 	);
-print(r);
 	r
 }
 

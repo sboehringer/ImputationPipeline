9e35c67276ddf032e8c34041c1dcde4fa216a1ed
Stefan Boehringer <github@s-boehringer.org>
2015-05-07 14:20:41 +0200

    - pullin modification

--- maintenance/pullin-sb.sh
+++ maintenance/pullin-sb.sh
@@ -2,3 +2,5 @@
 
 cp ~/src/Rprivate/*.R $PIPELINEBASE/R/Rprivate
 cp ~/src/scripts/System/qsub.pl $PIPELINEBASE/perl
+
+( cd ~/src/privatePerl ; cp TempFileNames.pm Set.pm PropertyList.pm $PIPELINEBASE/perl-lib )

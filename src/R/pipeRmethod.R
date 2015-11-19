6dddf99f26cf895a642cb8db91162607883d22d4
Stefan Boehringer <github@s-boehringer.org>
2015-11-19 14:38:06 +0100

    - debugging fix

--- src/R/pipeRmethod.R
+++ src/R/pipeRmethod.R
@@ -52,9 +52,8 @@ pipeRmethod = function(input, output, variableFile, pedFile, writeAsTable = T, d
 	Tfile = file(genotypeFile, "r")
 	Ifile = file(genotypeInfofile, "r")
 	infocols = scan(Ifile, what=character(0), n=10, quiet=T) #discard header
-browser();
 	r = lapply(1:N, function(i) {
-		if (!is.null(skipToAndBrowseAtLine) && skipToAndBrowseAtLine < i) return(NULL);
+		if (!is.null(skipToAndBrowseAtLine) && i < skipToAndBrowseAtLine) return(NULL);
 		firstcols = scan(Tfile, what = character(0), n = 5, quiet=T)
 		infocols = scan(Ifile, what = character(0), n = 10, quiet=T)
 		snpname = firstcols[2];

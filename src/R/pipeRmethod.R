dd6ef711a50fdcdeea21277ae4b3de6be7b5b2c7
Stefan Boehringer <github@s-boehringer.org>
2015-11-19 14:36:35 +0100

    - hard coded browser - entropyCuts

--- src/R/pipeRmethod.R
+++ src/R/pipeRmethod.R
@@ -11,7 +11,8 @@
 #pipeRmethod = function(input, output, phenos, covs, variableFile, pedFile, writeAsTable = T) {
 pipeRmethod = function(input, output, variableFile, pedFile, writeAsTable = T, digits = NULL, ...,
 	RfunctionSource, RfunctionName, prefixes = splitString(':', Sys.getenv('RSCRIPTS')),
-	by = NULL, do_debug = F, skipToAndBrowseAtLine = NULL, entropyLimit = 2e-3, entropyCuts = 4){
+	by = NULL, do_debug = F, skipToAndBrowseAtLine = NULL,
+	entropyLimit = 2e-3, entropyCuts = c(0, .5, 1.5, 1+1e-3)){
 	# <p> create data frame w/o genotypes
 	Log(sprintf("Trying to read variable file '%s'", variableFile), 2);
 	vars = readTable(variableFile);
@@ -51,6 +52,7 @@ pipeRmethod = function(input, output, variableFile, pedFile, writeAsTable = T, d
 	Tfile = file(genotypeFile, "r")
 	Ifile = file(genotypeInfofile, "r")
 	infocols = scan(Ifile, what=character(0), n=10, quiet=T) #discard header
+browser();
 	r = lapply(1:N, function(i) {
 		if (!is.null(skipToAndBrowseAtLine) && skipToAndBrowseAtLine < i) return(NULL);
 		firstcols = scan(Tfile, what = character(0), n = 5, quiet=T)
@@ -76,7 +78,7 @@ pipeRmethod = function(input, output, variableFile, pedFile, writeAsTable = T, d
 		
 		# <p> call function
 		if (!is.null(skipToAndBrowseAtLine) && skipToAndBrowseAtLine == i) browser();
-		gtCat = cut(data$MARKER_dosage, seq(0, 2 + 1e-3, length.out = entropyCuts), right = F);
+		gtCat = cut(data$MARKER_dosage, entropyCuts, right = F);
 		H = table.entropy(gtCat);
 		Log(sprintf('Calling %s for snp %s [#%d] [Entropy:%.1e]', RfunctionName, snpname, i, H), 5);
 		# <!> avoid analysis of degenerate data, cox-regression might core-dump
